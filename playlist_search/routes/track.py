b8a75ed5e48b5d61858c1871e035a62bbdbd275c
Alex Pogue <alex@epogue.com>
2019-08-30 16:41:24 -0500

    Rename api methods

--- playlist_search/routes/track.py
+++ playlist_search/routes/track.py
@@ -13,8 +13,7 @@ from .util import get_by_id
 track_blueprint = Blueprint('tracks', __name__)
 
 @track_blueprint.route('/')
-def get_tracks():
-    track = Track.query.first()
+def get_track_by_spotify_id():
     requested_spotify_id = request.args.get('spotify_id')
     if requested_spotify_id is not None:
         track = Track.query.filter_by(spotify_id=requested_spotify_id).first()
@@ -22,5 +21,5 @@ def get_tracks():
     return jsonify(result)
 
 @track_blueprint.route('/<int:track_id>', methods=['GET'])
-def get_track(track_id):
+def get_track_by_id(track_id):
     return get_by_id(Track, track_id, track_schema)
