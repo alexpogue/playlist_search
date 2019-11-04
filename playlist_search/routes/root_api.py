8481d0093d059e6e48b0399df1b57e1462559eb1
Alex Pogue <alex@epogue.com>
2019-11-04 01:00:18 -0600

    Add httpauth

--- playlist_search/routes/root_api.py
+++ playlist_search/routes/root_api.py
@@ -1,12 +1,28 @@
 import json
 
 from flask import Blueprint, request, abort, jsonify, render_template
+from flask_httpauth import HTTPBasicAuth
 from ..models.base import db
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_marshmallow import pprint
 
 root_api_blueprint = Blueprint('root_api', __name__)
 
+auth = HTTPBasicAuth()
+
+users = {
+    'hi': generate_password_hash('bye')
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
+
 @root_api_blueprint.route('/')
+@auth.login_required
 def get_goals():
     return render_template('index.html')
