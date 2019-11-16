6a64b6487f28827fd073f05cbeb5c4fb8504d04e
Alex Pogue <alex@epogue.com>
2019-11-15 22:03:29 -0600

    Add wsgi for gunicorn, update requirements.txt

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     # Google AppEngine sets PORT env var to say which port it wants us to run on
     PORT = os.environ.get('PORT')
     if PORT is not None:
-        app.run(port=PORT)
+        app.run(host='0.0.0.0', port=PORT)
     else:
-        app.run()
+        app.run(host='0.0.0.0')
