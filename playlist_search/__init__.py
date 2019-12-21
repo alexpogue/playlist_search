85b00b99332a31684575e4e8e015362de0bae4d9
Alex Pogue <alex@epogue.com>
2019-12-21 19:59:14 +0000

    Close db connection in celery postrun

--- playlist_search/__init__.py
+++ playlist_search/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, current_app
 
 import logging
 from logging.handlers import SysLogHandler
 
 from celery import Celery
+from celery.signals import task_postrun
 import config
 
 from .util import generate_config_file_from_heroku_env
@@ -11,7 +12,20 @@ import os
 import sys
 import config
 
+
 celery = Celery(__name__, backend=config.CELERY_RESULT_BACKEND, broker=config.CELERY_BROKER_URL)
+
+# Clean up sqlalchemy session after celery teardown (like flask-sqlalchemy would normally do)
+# from https://bl.ocks.org/twolfson/a1b329e9353f9b575131
+def handle_celery_postrun(retval=None, *args, **kwargs):
+    """After each Celery task, teardown our db session"""
+    if current_app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']:
+        if not isinstance(retval, Exception):
+            db.session.commit()
+    # If we aren't in an eager request (i.e. Flask will perform teardown), then teardown
+    if not celery.conf.task_always_eager:
+        from .models.base import db
+        db.session.remove()
 
 if 'RUNNING_ON_HEROKU' in os.environ and os.environ['RUNNING_ON_HEROKU'] == 'True':
     generate_config_file_from_heroku_env()
@@ -40,6 +54,7 @@ def create_app():
     app.logger.info('db uri = {}'.format(app.config['SQLALCHEMY_DATABASE_URI']))
 
     celery.conf.update(app.config)
+    task_postrun.connect(handle_celery_postrun)
 
     models.init_app(app)
     routes.init_app(app)
