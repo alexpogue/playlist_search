85d04422046717134cfabd0f48598a5c9125adb6
Alex Pogue <alex@epogue.com>
2019-12-28 13:48:59 -0600

    Add all playlists for total playlist count

--- playlist_search/routes/admin.py
+++ playlist_search/routes/admin.py
@@ -38,14 +38,17 @@ def reset_db_task(self):
 
     db.create_all()
 
+    user_playlist_counts = [get_user_playlist_count(u, spotify) for u in user_spotify_ids]
+    print('user_playlist_counts  = {}'.format(user_playlist_counts ))
+    total_playlist_count = sum(user_playlist_counts)
+    print('total_playlist_count  = {}'.format(total_playlist_count ))
+
+
+    cur_playlist = 0
     
     for user_spotify_id in user_spotify_ids:
         app.logger.info('user_spotify_id = {}'.format(user_spotify_id))
         
-        total_playlists = get_user_playlist_count(user_spotify_id, spotify)
-
-        i = 0
-
         api_playlists = get_user_playlists(user_spotify_id, spotify, test_mode=False)
         for api_playlist in api_playlists:
             api_playlist_snapshot_id = api_playlist['snapshot_id']
@@ -56,30 +59,30 @@ def reset_db_task(self):
             if db_playlist is not None:
                 if db_playlist.snapshot_id == api_playlist['snapshot_id']:
                     app.logger.info('found playlist already in database and up to date - skipping: {}'.format(playlist_spotify_id))
-                    i += 1
+                    cur_playlist += 1
                     self.update_state(state='PROGRESS',
-                                  meta={'current': i, 'total': total_playlists,
+                                  meta={'current': cur_playlist, 'total': total_playlist_count,
                                         'status': 'working'})
                     continue
                 else:
                     app.logger.info('found playlist already in playlist, but not up to date - updating: {}'.format(playlist_spotify_id))
                     db.session.delete(db_playlist)
                     store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                    i += 1
+                    cur_playlist += 1
                     self.update_state(state='PROGRESS',
-                                  meta={'current': i, 'total': total_playlists,
+                                  meta={'current': cur_playlist, 'total': total_playlist_count,
                                         'status': 'working'})
                     continue
             else:
                 app.logger.info('did not find playlist in database, creating a new one: {}'.format(playlist_spotify_id))
                 store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                i += 1
+                cur_playlist += 1
                 self.update_state(state='PROGRESS',
-                              meta={'current': i, 'total': total_playlists,
+                              meta={'current': cur_playlist, 'total': total_playlist_count,
                                     'status': 'working'})
                 continue
 
-    return {'current': total_playlists, 'total': total_playlists, 'status': 'completed'}
+    return {'current': total_playlist_count, 'total': total_playlist_count, 'status': 'completed'}
 
 def reset_db_no_celery():
     spotify = init_spotipy()
