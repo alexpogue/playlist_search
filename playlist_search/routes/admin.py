a67e1dac8447e5eaa7e814fecf6a0944a48217c3
Alex Pogue <alex@epogue.com>
2019-12-28 18:24:31 -0600

    Clean up duplicated code, add user to status

--- playlist_search/routes/admin.py
+++ playlist_search/routes/admin.py
@@ -31,7 +31,6 @@ def drop_db():
 
 @celery.task(bind=True)
 def reset_db_task(self):
-    # TODO: OUT OF SYNC WITH NO_CELERY VERSION -- DONT USE UNTIL RESYNCING WITH OTHER CODE
     spotify = init_spotipy()
 
     user_spotify_ids = ['particledetector', 'thesoundsofspotify']
@@ -42,7 +41,6 @@ def reset_db_task(self):
     print('user_playlist_counts  = {}'.format(user_playlist_counts ))
     total_playlist_count = sum(user_playlist_counts)
     print('total_playlist_count  = {}'.format(total_playlist_count ))
-
 
     cur_playlist = 0
     
@@ -59,63 +57,20 @@ def reset_db_task(self):
             if db_playlist is not None:
                 if db_playlist.snapshot_id == api_playlist['snapshot_id']:
                     app.logger.info('found playlist already in database and up to date - skipping: {}'.format(playlist_spotify_id))
-                    cur_playlist += 1
-                    self.update_state(state='PROGRESS',
-                                  meta={'current': cur_playlist, 'total': total_playlist_count,
-                                        'status': 'working'})
-                    continue
                 else:
                     app.logger.info('found playlist already in playlist, but not up to date - updating: {}'.format(playlist_spotify_id))
                     db.session.delete(db_playlist)
                     store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                    cur_playlist += 1
-                    self.update_state(state='PROGRESS',
-                                  meta={'current': cur_playlist, 'total': total_playlist_count,
-                                        'status': 'working'})
-                    continue
             else:
                 app.logger.info('did not find playlist in database, creating a new one: {}'.format(playlist_spotify_id))
                 store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                cur_playlist += 1
-                self.update_state(state='PROGRESS',
-                              meta={'current': cur_playlist, 'total': total_playlist_count,
-                                    'status': 'working'})
-                continue
+
+            cur_playlist += 1
+            self.update_state(state='PROGRESS',
+                          meta={'current': cur_playlist, 'total': total_playlist_count,
+                                'status': 'syncing_playlists_{}'.format(user_spotify_id)})
 
     return {'current': total_playlist_count, 'total': total_playlist_count, 'status': 'completed'}
-
-def reset_db_no_celery():
-    spotify = init_spotipy()
-
-    user_spotify_ids = ['particledetector', 'thesoundsofspotify']
-
-    db.create_all()
-
-    for user_spotify_id in user_spotify_ids:
-        app.logger.info('user_spotify_id = {}'.format(user_spotify_id))
-        api_playlists = get_user_playlists(user_spotify_id, spotify, test_mode=False)
-        for api_playlist in api_playlists:
-            api_playlist_snapshot_id = api_playlist['snapshot_id']
-            playlist_spotify_id = api_playlist['id']
-            app.logger.info('playlist_spotify_id  = {}'.format(playlist_spotify_id ))
-
-            db_playlist = Playlist.query.filter_by(spotify_id=playlist_spotify_id).first()
-            if db_playlist is not None:
-                if db_playlist.snapshot_id == api_playlist['snapshot_id']:
-                    app.logger.info('found playlist already in database and up to date - skipping: {}'.format(playlist_spotify_id))
-                    continue
-                else:
-                    app.logger.info('found playlist already in playlist, but not up to date - updating: {}'.format(playlist_spotify_id))
-                    db.session.delete(db_playlist)
-                    store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                    continue
-            else:
-                app.logger.info('did not find playlist in database, creating a new one: {}'.format(playlist_spotify_id))
-                store_playlist_and_subobjects_to_db(user_spotify_id, playlist_spotify_id, spotify)
-                continue
-
-    return {'current': total_playlists, 'total': total_playlists, 'status': 'completed'}
-
 
 
 @admin_blueprint.route('/reset_db', methods=['POST'])
