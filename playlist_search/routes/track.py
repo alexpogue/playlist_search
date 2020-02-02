04814df632d1d6fbfc308f05d8108c49c9092dc5
Alex Pogue <alex@epogue.com>
2020-02-02 14:56:10 -0600

    Bandaid fix for albums

--- playlist_search/routes/track.py
+++ playlist_search/routes/track.py
@@ -37,7 +37,8 @@ def get_track_csv_by_spotify_id():
     return track_in_csv
 
 def combine_info_for_track(spotify_id):
-    api_track = lookup_track(spotify_id, fields=['id'])
+    api_track = lookup_track(spotify_id, fields=['id', 'name', 'artists'])
+    api_track['album'] = {'name': 'unknown'}
 
     track_from_db = Track.query.filter_by(spotify_id=spotify_id).first()
     app.logger.info('track_from_db  = {}'.format(track_from_db ))
